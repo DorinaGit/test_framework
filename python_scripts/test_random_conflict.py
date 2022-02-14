1a7d45c6a2b94246b4034612b7a321a335277fdc
dorina-sfi <94060717+dorina-sfi@users.noreply.github.com>
2022-02-14 17:44:58 +0100

    Add files via upload

--- python_scripts/test_random_conflict.py
+++ python_scripts/test_random_conflict.py
@@ -1,90 +1,105 @@
 import csv
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import json
+import shutil
 import os
 
-#configJson = {"ARCH", "CONFIGFIX_TEST_PATH", "CONFIGFIX_TEST_CONFIG_DIR", "CONFIGFIX_TEST_PROBABILITY", "CONFIGFIX_TEST_MIN_CONFLICT_SIZE", "CONFIGFIX_TEST_MAX_CONFLICT_SIZE", "NO_CONFLICTS"}
-#architectureList = ["alpha", "arc", "arm", "arm64", "c6x", "csky", "h8300", "hexagon", "ia64", "m68k", "microblaze", "mips", "nds32", "nios2", "openrisc", "parisc", "powerpc", "riscv", "s390", "sh", "sh64", "sparc32", "sparc64", "um", "i386", "x86_64", "xtensa"]
-# def switch():
-# x = 3
-# for case in switch():
-#     if case(1):
-#         ARCH = "alpha"
-#         break
-#     elif case(2):
-#         ARCH = "arc"
-#         break
-#     else:
-#     print("Invalid choice")
-#
 
+#return current working directory
+CWD = os.getcwd()
+#get the bin path
+#BIN_PATH = os.path.abspath(os.path.join(CWD, "../bin"))
+BIN_PATH = "/home/dorina/CLionProjects/test_config/cmake-build-debug"
+BIN_NAME = "test_random_conflict"
 
-configJson = {}
-architectureList = ["arc"]
-i = 10
-# the json file where the output must be stored
-CONFIGFIX_ROOT = "/home/dorina/CLionProjects/TestRandomConflict/tests_59"
+print("1.Alpha 2.ARC 3.ARM 32-bit 4.ARM 64-bit 5.TINBSPTMS320C6x 6.C-SKY 7.H8/300 8.Qualcomm Hexagon 9.IA-64\n"
+      "10.Motorola 68000 11.MicroBlaze 12.MIPS 13.NDS32 14.Nios II 15.OpenRISC 16.PA-RISC 17.PowerPC 18.RISC-V\n"
+      "19.IBM System/390 and z/Architecture 20.SuperH 32-bit 21.SuperH 64-bit 22.SPARC 32-bit 23.SPARC 64-bit\n"
+      "24.User-Mode Linux 25.x86-32 26.x86-64 27.Xtensa")
 
-for arct in architectureList:
-    for prob in range(1, 2):
-        CONFIGFIX_TEST_CONFIG_DIR = "{}/{}/config.{}".format(CONFIGFIX_ROOT, arct, i * prob);
-        configJson["KERNELVERSTION"] = "5.9.6"
-        configJson["CC_VERSION_TEXT"] = "gcc (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
-        configJson["ARCH"] = arct
-        configJson["SRCARCH"] = arct
-        configJson["srctree"] = "."
-        configJson["CC"] = "gcc"
-        configJson["LD"] = "ld"
-        configJson["CONFIGFIX_TEST_PATH"] = CONFIGFIX_ROOT
-        configJson["CONFIGFIX_TEST_CONFIG_DIR"] = CONFIGFIX_TEST_CONFIG_DIR
-        configJson["CONFIG_BASE"] = ".config.base"
-        configJson["KCONFIG_CONFIG"] = "config."
-        configJson["CONFIGFIX_TEST_PROBABILITY"] = str(i * prob)
-        configJson["CONFIGFIX_TEST_MIN_CONFLICT_SIZE"] = 1
-        configJson["CONFIGFIX_TEST_MAX_CONFLICT_SIZE"] = 1
-        configJson["NO_CONFLICTS"] = 1
+choice = input("Please choose a architecture:")
+choice = int(choice)
+if choice == 1:
+    archName = "alpha"
+elif choice == 2:
+    archName = "arc"
+elif choice == 3:
+    archName = "arm"
+elif choice == 4:
+    archName = "arm64"
+elif choice == 5:
+    archName = "c6x"
+elif choice == 6:
+    archName = "csky"
+elif choice == 7:
+    archName = "h8300"
+elif choice == 8:
+    archName = "hexagon"
+elif choice == 9:
+    archName = "ia64"
+elif choice == 10:
+    archName = "m68k"
+elif choice == 11:
+    archName = "microblaze"
+elif choice == 12:
+    archName = "mips"
+elif choice == 13:
+    archName = "nds32"
+elif choice == 14:
+    archName = "nios2"
+elif choice == 15:
+    archName = "openrisc"
+elif choice == 16:
+    archName = "parisc"
+elif choice == 17:
+    archName = "powerpc"
+elif choice == 18:
+    archName = "riscv"
+elif choice == 19:
+    archName = "s390"
+elif choice == 20:
+    archName = "sh"
+elif choice == 21:
+    archName = "sh64"
+elif choice == 22:
+    archName = "sparc32"
+elif choice == 23:
+    archName = "sparc64"
+elif choice == 24:
+    archName = "um"
+elif choice == 25:
+    archName = "i386"
+elif choice == 26:
+    archName = "x86_64"
+elif choice == 27:
+    archName = "xtensa"
+else:
+    archName = "x86_64"
 
-        with open("config.json", "w") as out_file:
-            json.dump(configJson, out_file, indent=4)
-        ##out_file.close()
+NO_CONFIG = int(input("Please input how many number of configurations you want to run:") or "1")
 
-       ## os.system("./TestRandomConflicts --config config.json")
+#declare arguments for running the configfix_testing_framework script
+ARGS = "-s /home/dorina/CLionProjects/test_config/cmake-build-debug/Kconfig"
 
+for prob in range(1, NO_CONFIG+1):
+    CONFIG_FOLDER_NAME = "config"
+    path_prob = CONFIG_FOLDER_NAME + "." + str(prob * 10)
 
-#choose a style
-plt.style.use('classic')
-plt.style.use('fivethirtyeight')
+    config_path_initial = "/home/dorina/CLionProjects/test_config/tests/" + archName + "/" + path_prob + "/" + "config.json"
+    config_path_final = "/home/dorina/CLionProjects/test_config/cmake-build-debug/config.json"
 
-#define values
-ARCHITECTURE_IDX = 0
-CONFIG_IDX = 8
-CONFIG_TIME = 9
-my_data =[]
-my_data_01 =[]
+    if os.path.isfile(config_path_final):
+        os.remove(config_path_final)
+        shutil.copyfile(config_path_initial, config_path_final)
+    else:
+        shutil.copyfile(config_path_initial, config_path_final)
 
-TIME_RESOLUTION = 5
-#get values
-with open("../configfix_test/tests_59_new/results.csv", newline='\n') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    for row in reader:
-        architecture = row[ARCHITECTURE_IDX]
-        configuration = row[CONFIG_IDX]
-        time = float(row[CONFIG_TIME])
-        my_data.append(int(time/TIME_RESOLUTION)*TIME_RESOLUTION)
-        my_data_01.append(configuration)
-        print(architecture,configuration,time)
+    def run_generate(bin_path, bin_name):
+        # get generate_random_config from the bin path
+        bin_exec_path = os.path.join(bin_path, bin_name)
+        # go into the bin execute the arguments and the generate_random_config file
+        os.chdir(bin_path)
+        cmd = "{} {}".format(bin_exec_path, ARGS)
+        os.system(cmd)
 
-#print(", ".join(row));
-#print(row)
-
-# the data
-print(my_data)
-plt.hist(my_data, edgecolor='black', density=True)
-
-plt.xlabel('Time')
-plt.ylabel('Occurrence')
-plt.title('Histogram')
-#plt.xlim(0, 40)
-#plt.ylim(0, 11)
-
-plt.show()
+    run_generate(BIN_PATH, BIN_NAME)
